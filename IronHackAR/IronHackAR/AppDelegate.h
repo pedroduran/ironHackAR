9a33c0c95d5c044239226487cdd17364c1d97953
Pedro Durán <pedro.duranc@gmail.com>
2014-07-01 13:11:27 +0200

    Initial Commit

---
+++ IronHackAR/IronHackAR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IronHackAR
+//
+//  Created by zoompin iOS on 01/07/14.
+//  Copyright (c) 2014 zoompin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IronHackAR/IronHackAR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IronHackAR
+//
+//  Created by zoompin iOS on 01/07/14.
+//  Copyright (c) 2014 zoompin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
