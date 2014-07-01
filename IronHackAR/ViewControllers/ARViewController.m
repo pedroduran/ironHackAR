b7794e24642925974f1ddab709cbf8c583ff75c7
Pedro Durán <pedro.duranc@gmail.com>
2014-07-01 14:16:05 +0200

    Add ARchitechView to ARViewController

--- IronHackAR/ViewControllers/ARViewController.m
+++ IronHackAR/ViewControllers/ARViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ARViewController.h"
+#import <WikitudeSDK/WTArchitectView.h>
 
 @interface ARViewController ()
+
+@property (nonatomic, strong) WTArchitectView *architectView;
 
 @end
 
@@ -26,13 +29,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    [self initializeARView];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initializeARView {
+    if ( [WTArchitectView isDeviceSupportedForAugmentedRealityMode:WTAugmentedRealityMode_Both] ) { // 1
+        self.architectView = [[WTArchitectView alloc] initWithFrame:self.view.bounds motionManager:nil augmentedRealityMode:WTAugmentedRealityMode_Both];
+        [self.architectView setLicenseKey:kWT_LICENSE_KEY]; // 2
+        [self.view addSubview:self.architectView]; // 3
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.architectView start];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.architectView stop];
 }
 
 @end
